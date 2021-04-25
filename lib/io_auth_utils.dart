b0c9d660b0bc8c9f20968739597f2c8b09b14025
alex <alex@tekartik.com>
2021-04-25 08:03:57 +0200

    fix lint

--- lib/io_auth_utils.dart
+++ lib/io_auth_utils.dart
@@ -12,8 +12,8 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:fs_shim/utils/io/read_write.dart';
-import 'package:googleapis_auth/googleapis_auth.dart' as auth;
 import 'package:googleapis_auth/auth_io.dart' as auth;
+import 'package:googleapis_auth/googleapis_auth.dart' as auth;
 import 'package:http/http.dart' as http;
 import 'package:path/path.dart';
 import 'package:tekartik_io_utils/io_utils_import.dart';
