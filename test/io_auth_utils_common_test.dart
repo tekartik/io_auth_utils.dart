959059249e1fe9707b081be460c418f2c462277f
Alexandre Roux <alex@tekartik.com>
2024-07-20 13:03:14 +0200

    fix ci

--- test/io_auth_utils_common_test.dart
+++ test/io_auth_utils_common_test.dart
@@ -1,6 +1,6 @@
-import 'package:dev_test/test.dart';
 import 'package:tekartik_io_auth_utils/src/io_auth_utils_memory.dart';
 import 'package:tekartik_io_utils/io_utils_import.dart';
+import 'package:test/test.dart';
 
 // Copyright (c) 2017, alex. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
